9ed7f0ac7f99bcf66015ae02932b5be3a5eeee4a
SlackJK <87824853+SlackJK@users.noreply.github.com>
2022-08-13 00:23:02 +0200

    Initial commit

---
+++ src/test/java/com/slackjk/servermonitor/ServerMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slackjk.servermonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
