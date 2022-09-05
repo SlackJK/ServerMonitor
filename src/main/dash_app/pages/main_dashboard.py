0deec495e54739feb8ad74341d5b78dded0b2b25
AX-11 <daubner.andy@gmail.com>
2022-09-05 11:12:00 +0200

    gauge chart

--- src/main/dash_app/pages/main_dashboard.py
+++ src/main/dash_app/pages/main_dashboard.py
@@ -1,9 +1,22 @@
 import dash
 from dash import html, dcc, callback, Input, Output
+import plotly.express as px
+import plotly.graph_objs as go
 
 dash.register_page(__name__)
 
+fig = go.Figure(go.Indicator(
+    mode = "gauge+number",
+    value = 3605,
+    domain = {"x":[0, 1], "y":[0,1]},
+    title = {"text":"Memory usage"}
+))
+
+
 layout = html.Div([
     html.H1("this is a temp test page"),
-    html.P("this will be the main dashboard")
+    html.P("this will be the main dashboard"),
+    html.Div(
+        dcc.Graph(figure=fig)
+    )
 ])
