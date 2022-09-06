76c369b76d77388d915a7956b6c01d251d3ebbd0
AX-11 <daubner.andy@gmail.com>
2022-09-06 10:58:37 +0200

    grid layout added

--- src/main/dash_app/pages/gauge_test.py
+++ src/main/dash_app/pages/gauge_test.py
@@ -7,21 +7,43 @@ import plotly.io as pio
 dash.register_page(__name__)
 
 # TODO fix bs figure that doesnt work
+# arc path https://community.plotly.com/t/arc-shape-with-path/7205/5
 fig = {
-    "values": [4000, 2000, 1000, 1000],
-    "marker": {
-        "line": {
-            "width": 2,
-            "color": "black"
-        },
-    },
-    "hole": .75,
-    "type": "pie",
-    "direction": "clockwise",
-    "sort": False,
-    "showlegend": False,
+    "data": [{"type": "pie",
+              "values": [50, 20, 30],
+              "hole":.75,
+              "sort":False,
+              "direction": "clockwise",
+              "textinfo": ""
+              }]
 }
+"""
+Failed component prop type: Invalid component prop `figure` key `values` supplied to Graph.
+Bad object: {
+  "values": [
+    50,
+    30,
+    20
+  ],
+  "marker": {
+    "line": {
+      "width": 2,
+      "color": "black"
+    }
+  },
+  "hole": 0.75,
+  "type": "pie",
+  "direction": "clockwise",
+  "sort": false,
+  "showlegend": false
+}
+Valid keys: [
+  "data",
+  "layout",
+  "frames"
+]
 
+"""
 
 layout = html.Div([
     html.H1("this is a temp test page"),
--- src/main/dash_app/pages/main_dashboard.py
+++ src/main/dash_app/pages/main_dashboard.py
@@ -1,5 +1,6 @@
 import dash
 from dash import html, dcc, callback, Input, Output
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly.graph_objs as go
 
@@ -14,11 +15,29 @@ fig = go.Figure(go.Indicator(
 ))
 
 
-
 layout = html.Div([
     html.H1("this is a temp test page"),
     html.P("this will be the main dashboard"),
-    html.Div(
-        dcc.Graph(figure=fig)
-    )
+    html.Div([
+        dbc.Row(dbc.Col(html.Div("row 1"))),
+        dbc.Row(
+            [
+                dbc.Col(html.Div(dcc.Graph(figure=fig))),
+                dbc.Col(html.Div(dcc.Graph(figure=fig))),
+                dbc.Col(html.Div(dcc.Graph(figure=fig))),
+            ]
+        )
+    ])
+# html.Div(
+#     [
+#         dbc.Row(dbc.Col(html.Div("A single column"))),
+#         dbc.Row(
+#             [
+#                 dbc.Col(html.Div("One of three columns")),
+#                 dbc.Col(html.Div("One of three columns")),
+#                 dbc.Col(html.Div("One of three columns")),
+#             ]
+#         ),
+#     ])
 ])
+
